2878dce4c04730a9dabbb026ab92af4a31ee693c
erkam_home <erkam.uzuncayir@fauderfield.com>
2023-03-22 01:19:14 +0300

    Event Scriptable Objects and Event Listeners: Create scripts for event scriptable objects and event listeners

---
+++ Hex Survivors/Assets/_Scripts/Event/VoidEventListener.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// This class listens to the VoidEvent scriptable object.
+/// When the event raised, it can call the methods assigned to it.
+/// </summary>
+public class VoidEventListener : MonoBehaviour
+{
+    #region Fields
+    public VoidEvent gameEvent;
+    public UnityEvent response;
+    #endregion
+
+    #region Unity API
+    void OnEnable()
+    {
+        gameEvent.RegisterListener( this );
+    }
+
+    void OnDisable()
+    {
+        gameEvent.UnregisterListener( this );
+    }
+    #endregion
+
+    #region API
+    public void OnEventRaised()
+    {
+        response.Invoke();
+    }
+    #endregion
+}
