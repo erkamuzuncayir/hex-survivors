315cc8fee1e1f9af4262f8dbc7902e8d582eb7b1
H. Erkam Uzuncayir <94782219+erkamuzuncayir@users.noreply.github.com>
2023-04-30 10:41:31 +0300

    Create TileManager.cs

---
+++ Assets/_Scripts/TileManager.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using _Scripts.Extensions;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileManager : MonoBehaviour
+{
+    [SerializeField] private TileDictionary _tileDictionary;
+    [SerializeField] private Tilemap _tilemap;
+    
+    private void Awake()
+    {
+        _tileDictionary.Tiles.Clear();
+        
+        GetAllTiles(_tilemap);
+    }
+    
+    public void GetAllTiles(Tilemap tilemap)
+    {
+        var bounds = tilemap.cellBounds;
+
+        // loop over the bounds (from min to max) on both axes
+        for (int x = bounds.min.x; x < bounds.max.x; x++)
+        {
+            for (int y = bounds.min.y; y < bounds.max.y; y++)
+            {
+                var cellPosition = new Vector3Int(x, y, 0);
+                
+                var tile = tilemap.GetTile<GroundTile>(cellPosition);
+                var coord = new Vector2Int(x, y);
+
+                if (tile == null)
+                {
+                    continue;
+                }
+
+                _tileDictionary.Tiles.Add(new TileData(tile, coord));
+            }
+        }
+    }
+}
