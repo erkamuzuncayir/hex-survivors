f4acca325794d1cbbf536b12108ca0a135ac72c2
H. Erkam Uzuncayir <94782219+erkamuzuncayir@users.noreply.github.com>
2023-04-30 10:41:23 +0300

    Create TileDictionary.cs

---
+++ Assets/_Scripts/ScriptableObjects/TileDictionary.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "Tile Dictionary")]
+public class TileDictionary : ScriptableObject
+{
+    public List<TileData> Tiles = new List<TileData>();
+}
+
+[Serializable]
+public class TileData
+{
+    public TileData(GroundTile tile, Vector2Int coord)
+    {
+        Tile = tile;
+        TileCoord = coord;
+    }
+    
+    public GroundTile Tile;
+    public Vector2Int TileCoord;
+}
