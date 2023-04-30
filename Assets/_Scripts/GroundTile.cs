576ef99aa185ee3898f4ffec5d5e3bc9cce08885
H. Erkam Uzuncayir <94782219+erkamuzuncayir@users.noreply.github.com>
2023-04-30 10:42:08 +0300

    Update GroundTile.cs

--- Assets/_Scripts/GroundTile.cs
+++ Assets/_Scripts/GroundTile.cs
@@ -1,31 +1,91 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 [CreateAssetMenu(fileName = "GroundTile", menuName = "Scriptable Tiles/Ground Tile")]
 public class GroundTile : Tile
 {
-    #region Fields
+    private const float TileHalfHeight = 0.75f;
+    private const float TileHalfWidth = 0.5f;
+    private const int PossibleNeighborCount = 6; // Because it's hexagon.
 
+    public bool IsWalkable = true;
     public bool IsEmpty = true;
     public int MoveCost = 1;
 
-    #endregion
+    public GroundTile Connection { get; private set; }
+    public int GValue { get; private set; }
+    public int HValue { get; private set; }
+    public int FValue => GValue + HValue;
+    public void SetConnection(GroundTile groundTile) => Connection = groundTile;
+    public void SetGValue(int g) => GValue = g;
+    public void SetHValue(int h) => HValue = h;
 
-    #region API
+    public List<Vector3Int> GetNeighborPositions(Tilemap tilemap, Vector3 currentTilePosition)
+    {
+        List<Vector3Int> neighborPositions = new List<Vector3Int>();
 
-    #endregion
+        Vector3Int currentCoord = tilemap.WorldToCell(currentTilePosition);
 
-    #region Implementation
+        if (currentCoord.y % 2 == 0)
+        {
+            neighborPositions.Add(new Vector3Int( currentCoord.x - 1, currentCoord.y + 1 , 0));
+            neighborPositions.Add(new Vector3Int( currentCoord.x, currentCoord.y + 1 , 0));
+            neighborPositions.Add(new Vector3Int( currentCoord.x - 1, currentCoord.y, 0));
+            neighborPositions.Add(new Vector3Int( currentCoord.x + 1, currentCoord.y, 0));
+            neighborPositions.Add(new Vector3Int( currentCoord.x - 1, currentCoord.y - 1 , 0));
+            neighborPositions.Add(new Vector3Int( currentCoord.x, currentCoord.y - 1 , 0));
+        }
+        else
+        {
+            neighborPositions.Add(new Vector3Int( currentCoord.x, currentCoord.y + 1 , 0));
+            neighborPositions.Add(new Vector3Int( currentCoord.x + 1, currentCoord.y + 1 , 0));
+            neighborPositions.Add(new Vector3Int( currentCoord.x - 1, currentCoord.y, 0));
+            neighborPositions.Add(new Vector3Int( currentCoord.x + 1, currentCoord.y, 0));
+            neighborPositions.Add(new Vector3Int( currentCoord.x, currentCoord.y - 1 , 0));
+            neighborPositions.Add(new Vector3Int( currentCoord.x + 1, currentCoord.y - 1 , 0));
+        }
 
+        /*
+        var neighborTileCoord =
+            tilemap.WorldToCell(currentTilePosition + new Vector3(-TileHalfWidth, -TileHalfHeight, 0));
+        if (tilemap.HasTile(neighborTileCoord) && tilemap.GetTile<GroundTile>(neighborTileCoord).IsWalkable)
+        {
+            neighborTiles.Add(tilemap.GetTile<GroundTile>(tilemap.WorldToCell(neighborTileCoord)));
+        }
 
+        neighborTileCoord = tilemap.WorldToCell(currentTilePosition + new Vector3(-TileHalfWidth, TileHalfHeight, 0));
+        if (tilemap.HasTile(neighborTileCoord) && tilemap.GetTile<GroundTile>(neighborTileCoord).IsWalkable)
+        {
+            neighborTiles.Add(tilemap.GetTile<GroundTile>(tilemap.WorldToCell(neighborTileCoord)));
+        }
 
-    #endregion
+        neighborTileCoord = tilemap.WorldToCell(currentTilePosition + new Vector3(-(TileHalfWidth * 2), 0, 0));
+        if (tilemap.HasTile(neighborTileCoord) && tilemap.GetTile<GroundTile>(neighborTileCoord).IsWalkable)
+        {
+            neighborTiles.Add(tilemap.GetTile<GroundTile>(tilemap.WorldToCell(neighborTileCoord)));
+        }
 
-    #region Editor Only
+        neighborTileCoord = tilemap.WorldToCell(currentTilePosition + new Vector3(TileHalfWidth, -TileHalfHeight, 0));
+        if (tilemap.HasTile(neighborTileCoord) && tilemap.GetTile<GroundTile>(neighborTileCoord).IsWalkable)
+        {
+            neighborTiles.Add(tilemap.GetTile<GroundTile>(tilemap.WorldToCell(neighborTileCoord)));
+        }
 
-#if UNITY_EDITOR
-#endif
+        neighborTileCoord = tilemap.WorldToCell(currentTilePosition + new Vector3(TileHalfWidth, TileHalfHeight, 0));
+        if (tilemap.HasTile(neighborTileCoord) && tilemap.GetTile<GroundTile>(neighborTileCoord).IsWalkable)
+        {
+            neighborTiles.Add(tilemap.GetTile<GroundTile>(tilemap.WorldToCell(neighborTileCoord)));
+        }
 
-    #endregion
+        neighborTileCoord = tilemap.WorldToCell(currentTilePosition + new Vector3(TileHalfWidth * 2, 0, 0));
+        if (tilemap.HasTile(neighborTileCoord) && tilemap.GetTile<GroundTile>(neighborTileCoord).IsWalkable)
+        {
+            
+            neighborTiles.Add(tilemap.GetTile<GroundTile>(tilemap.WorldToCell(neighborTileCoord)));
+        }
+        */
+        return neighborPositions;
+    }
 }
-
