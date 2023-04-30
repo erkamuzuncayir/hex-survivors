6820a9d3ec3aaff24498bd85c720ff055ec85d69
H. Erkam Uzuncayir <94782219+erkamuzuncayir@users.noreply.github.com>
2023-04-30 10:42:48 +0300

    Update PlayerMovement.cs

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,32 +1,42 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private const float HexHalfHeight = 0.75f;
-    private const float HexHalfWidth = 0.5f;
+    private const float TileHalfHeight = 0.75f;
+    private const float TileHalfWidth = 0.5f;
+    [SerializeField] private Vector3SO _playerPositionSO;
+    [FormerlySerializedAs("_pathfinding")] [SerializeField] private PathfindingSystem _pathfindingSystem;
 
     [SerializeField] private Tilemap _tilemap;
 
-    private Vector3Int _destinationGridCoordinate;
-    private Vector3 _destinationWorldPosition;
-    private Vector3 _playerWorldPosition;
-    private Vector3Int _playerGridCoordinate;
+    private Vector3Int _destinationCoordinate;
+    private Vector3 _destinationPosition;
+    private Vector3 _playerPosition;
+    private Vector3Int _playerCoordinate;
 
     [SerializeField] private float _speed = 1f;
     [SerializeField] private bool _isMoving;
-    
+
+    private void Awake()
+    {
+        _playerPositionSO.value = transform.position;
+    }
+
     public void OnMouseClick(Vector2 mousePosition)
     {
         // Check for whether clicked position is a cell or not.
-        _playerGridCoordinate = _tilemap.WorldToCell(transform.position);
+        _playerCoordinate = _tilemap.WorldToCell(transform.position);
         if (!_isMoving && _tilemap.HasTile(_tilemap.WorldToCell(mousePosition)))
         {        
-            _destinationGridCoordinate = _tilemap.WorldToCell(mousePosition);
+            _destinationCoordinate = _tilemap.WorldToCell(mousePosition);
          
-            if (_playerGridCoordinate != _destinationGridCoordinate)
+            if (_playerCoordinate != _destinationCoordinate)
             {
                 StartCoroutine(Move());
                 _isMoving = true;
@@ -36,50 +46,60 @@ public class PlayerMovement : MonoBehaviour
     
     private IEnumerator Move()
     {   
-        _playerWorldPosition = transform.position;
-        _destinationWorldPosition = _tilemap.CellToWorld(_destinationGridCoordinate);
+        _playerPosition = transform.position;
+        _destinationPosition = _tilemap.CellToWorld(_destinationCoordinate);
+
+        List<Vector3Int> moves =
+            _pathfindingSystem.FindNextMove(_tilemap, _tilemap.WorldToCell(_playerPosition), _destinationCoordinate);
+
+        foreach (var m in moves)
+        {
+            yield return ContinuousMove(_tilemap.CellToWorld(m), _speed);
+        }
+        /*
         Vector3 moveDirection;
+
         
-
-        if (_playerWorldPosition.x > _destinationWorldPosition.x)
+        if (_playerPosition.x > _destinationPosition.x)
         {
-            if (_playerWorldPosition.y > _destinationWorldPosition.y)
-                moveDirection = new Vector3(-HexHalfWidth, -HexHalfHeight, 0);
-            else if (_playerWorldPosition.y < _destinationWorldPosition.y)
-                moveDirection = new Vector3(-HexHalfWidth, HexHalfHeight, 0);
+            if (_playerPosition.y > _destinationPosition.y)
+                moveDirection = new Vector3(-TileHalfWidth, -TileHalfHeight, 0);
+            else if (_playerPosition.y < _destinationPosition.y)
+                moveDirection = new Vector3(-TileHalfWidth, TileHalfHeight, 0);
             else
-                moveDirection = new Vector3(-(HexHalfWidth * 2), 0, 0);
+                moveDirection = new Vector3(-(TileHalfWidth * 2), 0, 0);
             
-            yield return ContinuousMove(transform.position, _playerWorldPosition + moveDirection, _speed);
+            yield return ContinuousMove(_playerPosition + moveDirection, _speed);
 
         }
-        else if (_playerWorldPosition.x < _destinationWorldPosition.x)
+        else if (_playerPosition.x < _destinationPosition.x)
         {
-            if (_playerWorldPosition.y > _destinationWorldPosition.y)
-                moveDirection = new Vector3(HexHalfWidth, -HexHalfHeight, 0);
-            else if (_playerWorldPosition.y < _destinationWorldPosition.y)
-                moveDirection = new Vector3(HexHalfWidth, HexHalfHeight, 0);
+            if (_playerPosition.y > _destinationPosition.y)
+                moveDirection = new Vector3(TileHalfWidth, -TileHalfHeight, 0);
+            else if (_playerPosition.y < _destinationPosition.y)
+                moveDirection = new Vector3(TileHalfWidth, TileHalfHeight, 0);
             else
-                moveDirection = new Vector3(HexHalfWidth * 2, 0, 0);
+                moveDirection = new Vector3(TileHalfWidth * 2, 0, 0);
 
-            yield return ContinuousMove(transform.position, _playerWorldPosition + moveDirection, _speed);
+            yield return ContinuousMove(_playerPosition + moveDirection, _speed);
 
         }
-        else if(Math.Abs(_playerWorldPosition.x - _destinationWorldPosition.x) < .05f)
+        else if(Math.Abs(_playerPosition.x - _destinationPosition.x) < .05f)
         {
-            if (_playerWorldPosition.y > _destinationWorldPosition.y)
-                moveDirection = new Vector3(HexHalfWidth, -HexHalfHeight, 0);
+            if (_playerPosition.y > _destinationPosition.y)
+                moveDirection = new Vector3(TileHalfWidth, -TileHalfHeight, 0);
             else
-                moveDirection = new Vector3(HexHalfWidth, HexHalfHeight, 0);
+                moveDirection = new Vector3(TileHalfWidth, TileHalfHeight, 0);
             
-            yield return ContinuousMove(transform.position, _playerWorldPosition + moveDirection, _speed);
+            yield return ContinuousMove(_playerPosition + moveDirection, _speed);
 
         }
-        
-        _playerWorldPosition = transform.position;
 
-        if (Vector3.Distance(_playerWorldPosition, _destinationWorldPosition) < .05f)
+        */
+        if (Vector3.Distance(_playerPosition, _destinationPosition) < .05f)
         {
+            _playerPosition = transform.position;
+            WritePlayerPositionToSO();
             _isMoving = false;
         }
         else
@@ -89,7 +109,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private IEnumerator ContinuousMove(Vector3 start, Vector3 end, float speed)
+    private IEnumerator ContinuousMove(Vector3 end, float speed)
     {
         float t = 0;
         while (t < 1)
@@ -100,5 +120,10 @@ public class PlayerMovement : MonoBehaviour
         }
 
         transform.position = end;
+    }
+
+    public void WritePlayerPositionToSO()
+    {
+        _playerPositionSO.value = transform.position;
     }
 }
