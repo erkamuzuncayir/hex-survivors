99582c6b6ea216b6ea933d9584d42045f3ce9000
erkam_home <erkam.uzuncayir@fauderfield.com>
2023-05-03 14:25:29 +0300

    Create RuntimeSet script for Enemy script

---
+++ Assets/_Scripts/ScriptableObjects/EnemyRuntimeSet.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyRuntimeSet", menuName = "RuntimeSet/Enemy Script")]
+public class EnemyRuntimeSet : RuntimeSet<Enemy>
+{
+}
