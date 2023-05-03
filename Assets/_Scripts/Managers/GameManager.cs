194156ead5852f7069d2ccf3b23409c5c7708144
erkam_home <erkam.uzuncayir@fauderfield.com>
2023-05-03 14:26:52 +0300

    Modify GameManager.cs

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -2,8 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private StateSO _gameState;
+    [SerializeField] private Tilemap _tilemap;
+    [SerializeField] private MovementSystemSO _movementSystem;
+    private void Awake()
+    {
+        _movementSystem.Init(_tilemap);
+    }
 }
