92034fa207faeb488d1677810c42eb3a09d051a9
H. Erkam Uzuncayir <94782219+erkamuzuncayir@users.noreply.github.com>
2023-05-03 16:47:55 +0300

    Add initializing of PathfindingSystem into GameManager.cs

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -9,8 +9,12 @@ public class GameManager : MonoBehaviour
     [SerializeField] private StateSO _gameState;
     [SerializeField] private Tilemap _tilemap;
     [SerializeField] private MovementSystemSO _movementSystem;
+    [SerializeField] private PathfindingSystemSO _pathfindingSystem;
+
+    
     private void Awake()
     {
         _movementSystem.Init(_tilemap);
+        _pathfindingSystem.Init(_tilemap);
     }
 }
