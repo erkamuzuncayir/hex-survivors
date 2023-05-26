f755044cbec7971ac129be9cc632ecbc175aead3
erkam_desktop <erkamuzuncayir@gmail.com>
2023-05-27 00:40:20 +0300

    Create new type of data SO, Vector3IntSO.cs

---
+++ Assets/_Scripts/Data/Type/Vector3IntSO.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Data.Type
+{
+    [CreateAssetMenu(fileName = "Vector3IntSO", menuName = "Variables/Vector 3 Int")]
+    public class Vector3IntSO : DataSO<Vector3Int>
+    {
+    }
+}
